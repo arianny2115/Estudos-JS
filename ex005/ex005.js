4f43953ecf6b9c234079af20d66a702749a7851a
arianny2115 <ariannyayres8@gmail.com>
2023-09-12 19:05:49 -0300

    ex005

---
+++ ex005/ex005.js
@@ -0,0 +1,6 @@
+var vel = 58 
+console.log(`A velocidade do se carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+} // Condição simples
+console.log('Diriga sempre usando cinto de segurança')

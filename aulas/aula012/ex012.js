26c6a1136b31a5a0e31fd5196950fea85ee5e5ff
arianny2115 <ariannyayres8@gmail.com>
2023-09-13 14:51:47 -0300

    final ex0012

---
+++ aulas/aula012/ex012.js
@@ -0,0 +1,15 @@
+var agora = new Date()
+var hora = agora.getHours(
+    
+)
+console.log(`Agora são exatamente ${hora} horas` )
+if (hora < 12) {
+    console.log('Bom dia!')
+} else if(hora <= 18) {
+    console.log('Boa tarde!')
+} else if (hora >= 0 && hora <= 5) {
+    console.log*('Boa Madrugada!')
+} else {
+    console.log('Boa noite!')
+}
+ 

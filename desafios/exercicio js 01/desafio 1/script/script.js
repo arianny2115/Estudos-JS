dc26d29400ef50d7a349a29862179581fd9ca795
arianny2115 <ariannyayres8@gmail.com>
2023-09-14 18:14:28 -0300

    continuação des002

--- desafios/exercicio js 01/desafio 1/script/script.js
+++ desafios/exercicio js 01/desafio 1/script/script.js
@@ -1,1 +1,22 @@
-alert('Olá')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('image')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = ` Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#e6de97'
+    } else if (hora > 12 && hora <18) {
+        //BOA TARDE!
+        document.body.style.background = '#ef994c'
+        img.src = 'imagens/tarde.png'
+            } else {
+        // BOA NOITE!
+        img.src = "imagens/noite.png"
+        document.body.style.background = '#436e79'
+    }
+}
+

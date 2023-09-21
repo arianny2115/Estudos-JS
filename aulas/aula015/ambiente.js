31e91a26907d14d17cad12d04610f74d7a03447a
arianny2115 <ariannyayres8@gmail.com>
2023-09-21 16:41:52 -0300

    primeiro teste com vetor aula 15

---
+++ aulas/aula015/ambiente.js
@@ -0,0 +1,6 @@
+let num = [5, 8, 2, 9, 3]
+num.push(1)
+console.log (num)
+console.log(`O vetor em ordem crescente é ${num.sort()}`)
+console.log(`O vetor tem ${num.length} posições`)
+console.log (`O primeiro valor do vetor é ${num[0]}`)

deadf4fdb52018b5d796d15d699ef2954ffa7709
arianny2115 <ariannyayres8@gmail.com>
2023-09-21 15:38:08 -0300

    FIM EX004

--- desafios/exercicio js 02/desafio 04/script/script.js
+++ desafios/exercicio js 02/desafio 04/script/script.js
@@ -1,1 +1,17 @@
-alert('Olá')
+function contar() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length ==0) {
+        alert('Por favor, digite um número!')
+    }else {
+        let n = Number(num.value)
+        tab.innerHTML = ""
+        for(c = 1; c <=10; c++) {
+            let item = document.createElement('option')
+            item.text = ` ${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+
+}

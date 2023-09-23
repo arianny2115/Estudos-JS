6772b3862d4359bc33fda7777213c311b61c0fa6
arianny2115 <ariannyayres8@gmail.com>
2023-09-23 20:50:42 -0300

    .......

--- desafios/exercicio js 03/desafio05/script/script.js
+++ desafios/exercicio js 03/desafio05/script/script.js
@@ -1,1 +1,10 @@
-alert('Olá')
+function analizar(n){
+    var txtn = document.getElementById('txtn')
+    var sele = document.getElementById('seltab')
+    var numero = Number(txtn.value)
+    if (txtn.value == 0 || txtn.value > 100|| txtn.value.length == 0) {
+        alert('Valor é invalido ou ja encontrado na lista')
+    } else {
+        for ()
+    }
+}

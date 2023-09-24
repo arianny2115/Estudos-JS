8ffcfe17cea0bec0b4dd0caccd18100f44bd3817
arianny2115 <ariannyayres8@gmail.com>
2023-09-24 11:39:40 -0300

    ................

--- desafios/exercicio js 03/desafio05/script/script.js
+++ desafios/exercicio js 03/desafio05/script/script.js
@@ -1,10 +1,19 @@
-function analizar(n){
+function analizar(){
+
     var txtn = document.getElementById('txtn')
     var sele = document.getElementById('seltab')
     var numero = Number(txtn.value)
     if (txtn.value == 0 || txtn.value > 100|| txtn.value.length == 0) {
         alert('Valor é invalido ou ja encontrado na lista')
-    } else {
-        for ()
+    } else {                                                      
+        let nums = [ ]
+        
+        var c = numero
+        let item = document.createElement('option')
+        sele.appendChild(item)
+        item.text= c
+        
     }
+
+
 }
